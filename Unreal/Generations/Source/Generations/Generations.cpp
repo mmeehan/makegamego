3b3100e0066313c1316ff9410bb8f33c2ca23dfc
Matt Meehan <meehan.matt@gmail.com>
2016-08-30 21:44:19 -0700

    Empty unreal project and .gitignore updates

---
+++ Unreal/Generations/Source/Generations/Generations.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Generations.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Generations, "Generations" );
---
+++ Unreal/Generations/Source/Generations/GenerationsGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Generations.h"
+#include "GenerationsGameMode.h"
+
+
+
+
